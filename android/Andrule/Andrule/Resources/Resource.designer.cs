c93698e568cd93548c4445cb7b856df2b15d4fde
Olesia Kochetkova <KochetkovaOA@yandex.ru>
2016-03-20 00:04:14 +0300

    img resources update

--- android/Andrule/Andrule/Resources/Resource.designer.cs
+++ android/Andrule/Andrule/Resources/Resource.designer.cs
@@ -41,17 +41,90 @@ namespace Andrule
 			}
 		}
 		
+		public partial class Drawable
+		{
+			
+			// aapt resource value: 0x7f020000
+			public const int arrowLeft = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int arrowRight = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int backgnd = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int calibrateActive = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int CalibrateButton = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int calibrateScale = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int calibrateScaleActiveLine = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int calibrateUnselected = 2130837511;
+			
+			// aapt resource value: 0x7f020008
+			public const int connectButton = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int greenStatus = 2130837513;
+			
+			// aapt resource value: 0x7f02000a
+			public const int redStatus = 2130837514;
+			
+			// aapt resource value: 0x7f02000b
+			public const int setupActive = 2130837515;
+			
+			// aapt resource value: 0x7f02000c
+			public const int setupUnselected = 2130837516;
+			
+			// aapt resource value: 0x7f02000d
+			public const int slider1 = 2130837517;
+			
+			// aapt resource value: 0x7f02000e
+			public const int slider2 = 2130837518;
+			
+			// aapt resource value: 0x7f02000f
+			public const int themeActive = 2130837519;
+			
+			// aapt resource value: 0x7f020010
+			public const int themeUnselected = 2130837520;
+			
+			// aapt resource value: 0x7f020011
+			public const int touchRoom = 2130837521;
+			
+			// aapt resource value: 0x7f020012
+			public const int wheelActive = 2130837522;
+			
+			// aapt resource value: 0x7f020013
+			public const int wheelUnselected = 2130837523;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050002
-			public const int connectButton = 2131034114;
+			// aapt resource value: 0x7f060002
+			public const int connectButton = 2131099650;
 			
-			// aapt resource value: 0x7f050001
-			public const int editIpText = 2131034113;
+			// aapt resource value: 0x7f060001
+			public const int editIpText = 2131099649;
 			
-			// aapt resource value: 0x7f050000
-			public const int linearLayout1 = 2131034112;
+			// aapt resource value: 0x7f060000
+			public const int linearLayout1 = 2131099648;
 			
 			static Id()
 			{
@@ -66,14 +139,14 @@ namespace Andrule
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int Main = 2130968576;
 			
-			// aapt resource value: 0x7f030001
-			public const int SetupLayout = 2130903041;
+			// aapt resource value: 0x7f040001
+			public const int SetupLayout = 2130968577;
 			
-			// aapt resource value: 0x7f030002
-			public const int TabitemLayout = 2130903042;
+			// aapt resource value: 0x7f040002
+			public const int TabitemLayout = 2130968578;
 			
 			static Layout()
 			{
@@ -88,8 +161,8 @@ namespace Andrule
 		public partial class Mipmap
 		{
 			
-			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			// aapt resource value: 0x7f030000
+			public const int Icon = 2130903040;
 			
 			static Mipmap()
 			{
@@ -104,11 +177,11 @@ namespace Andrule
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
+			// aapt resource value: 0x7f050001
+			public const int app_name = 2131034113;
 			
-			// aapt resource value: 0x7f040000
-			public const int hello = 2130968576;
+			// aapt resource value: 0x7f050000
+			public const int hello = 2131034112;
 			
 			static String()
 			{
