580b3f619f02d989d711e1e53e47decf9cc69583
Salakhov Artur <gsmilexd@gmail.com>
2016-03-19 22:21:00 +0300

    Connect and disconnect by button

--- android/Andrule/Andrule/Resources/Resource.designer.cs
+++ android/Andrule/Andrule/Resources/Resource.designer.cs
@@ -44,11 +44,14 @@ namespace Andrule
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050002
-			public const int CoordinateText = 2131034114;
+			// aapt resource value: 0x7f050004
+			public const int ConnectBtn = 2131034116;
 			
 			// aapt resource value: 0x7f050003
-			public const int SendData = 2131034115;
+			public const int ConnectIpText = 2131034115;
+			
+			// aapt resource value: 0x7f050002
+			public const int CoordinateText = 2131034114;
 			
 			// aapt resource value: 0x7f050000
 			public const int linearLayout1 = 2131034112;
