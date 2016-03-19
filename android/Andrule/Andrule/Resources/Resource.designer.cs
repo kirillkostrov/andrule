f110a6a98b7fa13ce7e03dd1b587ef55d1f65932
Salakhov Artur <gsmilexd@gmail.com>
2016-03-19 23:20:26 +0300

    change logic into setupactivity

--- android/Andrule/Andrule/Resources/Resource.designer.cs
+++ android/Andrule/Andrule/Resources/Resource.designer.cs
@@ -44,20 +44,14 @@ namespace Andrule
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050004
-			public const int ConnectBtn = 2131034116;
-			
-			// aapt resource value: 0x7f050003
-			public const int ConnectIpText = 2131034115;
-			
 			// aapt resource value: 0x7f050002
-			public const int CoordinateText = 2131034114;
+			public const int connectButton = 2131034114;
+			
+			// aapt resource value: 0x7f050001
+			public const int editIpText = 2131034113;
 			
 			// aapt resource value: 0x7f050000
 			public const int linearLayout1 = 2131034112;
-			
-			// aapt resource value: 0x7f050001
-			public const int linearLayout2 = 2131034113;
 			
 			static Id()
 			{
@@ -74,6 +68,12 @@ namespace Andrule
 			
 			// aapt resource value: 0x7f030000
 			public const int Main = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int SetupLayout = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int TabitemLayout = 2130903042;
 			
 			static Layout()
 			{
--- android/Andrule/Andrule/Views/Tabs/SetupActivity.cs
+++ android/Andrule/Andrule/Views/Tabs/SetupActivity.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using Android.App;
+using Android.Hardware;
 using Android.OS;
 using Android.Widget;
 using Andrule.Network;
@@ -7,13 +9,16 @@ using Andrule.Network;
 namespace Andrule.View
 {
     [Activity]
-    public class SetupActivity : Activity
+    public class SetupActivity : Activity, ISensorEventListener
     {
         private NetWorkHelper netWorkHelper;
         private TextView textview;
         private EditText editIpText;
         private Button connectButton;
         private bool _isConnected;
+
+        private Sensor _sensor;
+        private SensorManager _sensorManager;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -24,6 +29,9 @@ namespace Andrule.View
             editIpText = FindViewById<EditText>(Resource.Id.editIpText);
             connectButton = FindViewById<Button>(Resource.Id.connectButton);
             connectButton.Click += GetIpAndConnect;
+
+            _sensorManager = (SensorManager)GetSystemService(SensorService);
+            _sensor = _sensorManager.GetDefaultSensor(SensorType.Accelerometer);
 
         }
 
@@ -38,6 +46,18 @@ namespace Andrule.View
             }
         }
 
+        protected override void OnResume()
+        {
+            _sensorManager.RegisterListener(this, _sensor, SensorDelay.Ui);
+            base.OnResume();
+
+        }
+        protected override void OnPause()
+        {
+            base.OnPause();
+            _sensorManager.UnregisterListener(this);
+        }
+
         private void CloseConnection(object sender, EventArgs e)
         {
             netWorkHelper.CloseConnection();
@@ -45,6 +65,27 @@ namespace Andrule.View
             connectButton.Click -= CloseConnection;
             connectButton.Text = "Connect";
             _isConnected = false;
+        }
+
+        public void OnAccuracyChanged(Sensor sensor, SensorStatus accuracy)
+        {
+
+        }
+
+        private void SendData(IReadOnlyList<int> sensorData)
+        {
+            netWorkHelper.Send(string.Format("^{0}|{1}|{2}|1|1|1$", sensorData[0], sensorData[1], sensorData[2]));
+        }
+
+        public void OnSensorChanged(SensorEvent e)
+        {
+            if (_isConnected)
+            {
+
+                var rotation = (int)(e.Values[1] * 3000 + 30000);
+                var data = new List<int> { rotation, 100, 200 };
+                SendData(data);
+            }
         }
     }
 }
