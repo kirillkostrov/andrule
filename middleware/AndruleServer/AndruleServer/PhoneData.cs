2771e09ab7aec25838d8530fe20860a93a319952
Olesia Kochetkova <KochetkovaOA@yandex.ru>
2016-03-19 14:59:25 +0300

    add model

---
+++ middleware/AndruleServer/AndruleServer/PhoneData.cs
@@ -0,0 +1,20 @@
+﻿namespace AndruleServer
+{
+    public class PhoneData
+    {
+        public int AxisX { get; set; }
+
+        public int AxisY { get; set; }
+
+        public int AxisZ { get; set; }
+
+        public bool Button1 { get; set; }
+
+        public bool Button2 { get; set; }
+        
+        public bool Button3 { get; set; }
+
+        public bool Button4 { get; set; }
+
+    }
+}
