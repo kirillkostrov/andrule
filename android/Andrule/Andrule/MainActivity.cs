44ea369ed7b9209d79fd7a2966ae17f12b67d578
Salakhov Artur <gsmilexd@gmail.com>
2016-03-19 17:34:00 +0300

    show coordinates

--- android/Andrule/Andrule/MainActivity.cs
+++ android/Andrule/Andrule/MainActivity.cs
@@ -1,28 +1,51 @@
 ﻿using Android.App;
+using Android.Content;
+using Android.Hardware;
 using Android.Widget;
 using Android.OS;
 
 namespace Andrule
 {
-	[Activity (Label = "Andrule", MainLauncher = true, Icon = "@mipmap/icon")]
-	public class MainActivity : Activity
+	[Activity (Label = "Andrule", MainLauncher = true, Icon = "@mipmap/icon", ScreenOrientation = Android.Content.PM.ScreenOrientation.Landscape)]
+	public class MainActivity : Activity, ISensorEventListener
 	{
 		int count = 1;
+        static readonly object _syncLock = new object();
+        TextView _sensorTextView;
+        SensorManager _sensorManager;
 
-		protected override void OnCreate (Bundle savedInstanceState)
+        protected override void OnCreate (Bundle bundle)
 		{
-			base.OnCreate (savedInstanceState);
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            _sensorManager = (SensorManager)GetSystemService(Context.SensorService);
+            _sensorTextView = FindViewById<TextView>(Resource.Id.CoordinateText);
+        }
 
-			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
+        protected override void OnResume()
+        {
+            base.OnResume();
+            _sensorManager.RegisterListener(this,
+                                            _sensorManager.GetDefaultSensor(SensorType.Accelerometer),
+                                            SensorDelay.Ui);
+        }
+        protected override void OnPause()
+        {
+            base.OnPause();
+            _sensorManager.UnregisterListener(this);
+        }
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
+        public void OnAccuracyChanged(Sensor sensor, SensorStatus accuracy)
+		{
+			// We don't want to do anything here.
+		}
+
+		public void OnSensorChanged(SensorEvent e)
+		{
+			lock (_syncLock)
+			{
+				_sensorTextView.Text = string.Format("x={0:f}, y={1:f}, y={2:f}", e.Values[0], e.Values[1], e.Values[2]);
+			}
 		}
 	}
 }
--- android/Andrule/Andrule/Resources/Resource.designer.cs
+++ android/Andrule/Andrule/Resources/Resource.designer.cs
@@ -41,27 +41,11 @@ namespace Andrule
 			}
 		}
 		
-		public partial class Drawable
-		{
-			
-			// aapt resource value: 0x7f020000
-			public const int monoandroidsplash = 2130837504;
-			
-			static Drawable()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Drawable()
-			{
-			}
-		}
-		
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f070000
-			public const int myButton = 2131165184;
+			// aapt resource value: 0x7f050000
+			public const int CoordinateText = 2131034112;
 			
 			static Id()
 			{
@@ -76,8 +60,8 @@ namespace Andrule
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f040000
-			public const int Main = 2130968576;
+			// aapt resource value: 0x7f030000
+			public const int Main = 2130903040;
 			
 			static Layout()
 			{
@@ -92,8 +76,8 @@ namespace Andrule
 		public partial class Mipmap
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int Icon = 2130903040;
+			// aapt resource value: 0x7f020000
+			public const int Icon = 2130837504;
 			
 			static Mipmap()
 			{
@@ -108,11 +92,11 @@ namespace Andrule
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f050001
-			public const int app_name = 2131034113;
+			// aapt resource value: 0x7f040001
+			public const int app_name = 2130968577;
 			
-			// aapt resource value: 0x7f050000
-			public const int hello = 2131034112;
+			// aapt resource value: 0x7f040000
+			public const int hello = 2130968576;
 			
 			static String()
 			{
@@ -120,22 +104,6 @@ namespace Andrule
 			}
 			
 			private String()
-			{
-			}
-		}
-		
-		public partial class Style
-		{
-			
-			// aapt resource value: 0x7f060000
-			public const int Mono_Android_Theme_Splash = 2131099648;
-			
-			static Style()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Style()
 			{
 			}
 		}
