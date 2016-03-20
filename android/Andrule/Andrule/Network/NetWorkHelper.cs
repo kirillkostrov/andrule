fbe1b1fdff1f547a2e1ad80b3e59815116f34d9b
Kirill Kostrov <kira.xtec@gmail.com>
2016-03-20 09:38:12 +0300

    some network refinements

--- android/Andrule/Andrule/Network/NetWorkHelper.cs
+++ android/Andrule/Andrule/Network/NetWorkHelper.cs
@@ -14,19 +14,32 @@ namespace Andrule.Network
         private static UdpClient _client;
         public static bool IsConnected { get; private set; }
         private static string currentIp { get; set; }
+        private static byte ReconnectCounter { get; set; }
 
         public static void Connect(string ip)
         {
             try
             {
                 _client = new UdpClient();
-                // KK's address
-                ip = "192.168.34.146";
-                //ip = "192.168.137.1";
+                
+                if (string.IsNullOrEmpty(ip)) {
+                    // KK's address
+                    ip = "192.168.34.146";
+                    //ip = "192.168.137.1";
+                }
 
                 _client.Connect(ip, 51515);
                 currentIp = ip;
                 IsConnected = true;
+                ReconnectCounter = 0;
+            }
+            catch (SocketException e)
+            {
+                IsConnected = false;
+                Log.Debug("Connection error: ", e.Message);
+                if (ReconnectCounter > 10) throw;
+                Reconnect();
+                ReconnectCounter++;
             }
             catch (Exception e)
             {
