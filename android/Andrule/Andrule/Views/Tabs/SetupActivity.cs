7e9b57d8e31ba825acfe6c5b97be124414f00301
Salakhov Artur <gsmilexd@gmail.com>
2016-03-20 10:37:21 +0300

    save ipAddress

--- android/Andrule/Andrule/Views/Tabs/SetupActivity.cs
+++ android/Andrule/Andrule/Views/Tabs/SetupActivity.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using Android.App;
+using Android.Content;
 using Android.Hardware;
 using Android.OS;
 using Android.Widget;
@@ -13,12 +14,15 @@ namespace Andrule.Views
     [Activity]
     public class SetupActivity : Activity
     {
+        private ISharedPreferences preferences;
+
         private NetWorkHelper netWorkHelper;
         private EditText editIpText;
         private Button connectButton;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            preferences = GetPreferences(FileCreationMode.Private);
             netWorkHelper = new NetWorkHelper();
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.SetupLayout);
@@ -26,13 +30,21 @@ namespace Andrule.Views
             editIpText = FindViewById<EditText>(Resource.Id.editIpText);
             connectButton = FindViewById<Button>(Resource.Id.connectButton);
             connectButton.Click += GetIpAndConnect;
+
+            var ipAddress = preferences.GetString("ipAddress", string.Empty);
+            editIpText.Text = ipAddress;
         }
 
         private void GetIpAndConnect(object sender, EventArgs e)
         {
             try
             {
-                NetWorkHelper.Connect(editIpText.Text);
+                var ipAddress = editIpText.Text;
+                var editor = preferences.Edit();
+                editor.PutString("ipAddress", ipAddress);
+                editor.Commit();
+
+                NetWorkHelper.Connect(ipAddress);
             }
             catch (Exception ex)
             {
