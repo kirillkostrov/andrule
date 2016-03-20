a8a3f9f92b81c63e05d9e71a5d458ca47c51a484
Константин <mrtaracqk@gmail.com>
2016-03-20 06:30:03 +0300

    listen seekBars changed

--- android/Andrule/Andrule/Views/Tabs/WheelActivity.cs
+++ android/Andrule/Andrule/Views/Tabs/WheelActivity.cs
@@ -2,34 +2,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using Andrule.Views;
 using Android.App;
 using Android.Hardware;
 using Android.OS;
 using Andrule.Accelerometer;
 using Andrule.Network;
 using Andrule.UIDetails;
+using Android.Content;
+using Android.Widget;
 
 namespace Andrule.Views
 {
     [Activity]
-    public class WheelActivity : Activity
+	public class WheelActivity : Activity
     {
         private AccelerometerListener _accelerometerListener;
+		private int progressBrake;
+		private int progressRun;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.WheelLayout);
+			progressBrake = 0;
+			progressRun = 0;
+
             var sensorManager = (SensorManager)GetSystemService(SensorService);
             _accelerometerListener = new AccelerometerListener(this, sensorManager, OnUpdateAccelerometer);
+
+			var _seekBarBrake = FindViewById<SeekBar> (Resource.Id.seekBarBrake);
+			_seekBarBrake.ProgressChanged += seekBarBrakeProgressChanged;
+			var _seekBarRun = FindViewById<SeekBar> (Resource.Id.seekBarRun);
+			_seekBarRun.ProgressChanged += seekBarRunProgressChanged;
         }
 
         public bool OnUpdateAccelerometer(Accelerometer.Accelerometer accelerometer)
         {
             try
             {
-                NetWorkHelper.Send(string.Format("^{0}|{1}|{2}|0|0|0|0$", accelerometer.Rotation, accelerometer.Throttle, accelerometer.ZAxis));
+				NetWorkHelper.Send(string.Format("^{0}|{1}|{2}|0|0|0|0$", accelerometer.Rotation, progressBrake, progressRun));
             }
             catch (Exception ex)
             {
@@ -51,6 +63,18 @@ namespace Andrule.Views
             _accelerometerListener.Stop();
         }
 
+		private void seekBarBrakeProgressChanged(object sender, SeekBar.ProgressChangedEventArgs e){
+			if (e.FromUser)
+			{
+				progressBrake = e.Progress * 1000;
+			}
+		}
 
+		private void seekBarRunProgressChanged(object sender, SeekBar.ProgressChangedEventArgs e){
+			if (e.FromUser)
+			{
+				progressRun = e.Progress * 1000;
+			}
+		}
     }
 }
