02e23b896b10307fd32e6620a7c88f0eb3193557
Olesia Kochetkova <KochetkovaOA@yandex.ru>
2016-03-20 06:52:27 +0300

    wheel button click action

--- android/Andrule/Andrule/Views/Tabs/WheelActivity.cs
+++ android/Andrule/Andrule/Views/Tabs/WheelActivity.cs
@@ -18,8 +18,14 @@ namespace Andrule.Views
 	public class WheelActivity : Activity
     {
         private AccelerometerListener _accelerometerListener;
+
 		private int progressBrake;
 		private int progressRun;
+
+		private int _button01Clicked;
+		private int _button02Clicked;
+		private int _button03Clicked;
+		private int _button04Clicked;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -35,13 +41,29 @@ namespace Andrule.Views
 			_seekBarBrake.ProgressChanged += seekBarBrakeProgressChanged;
 			var _seekBarRun = FindViewById<SeekBar> (Resource.Id.seekBarRun);
 			_seekBarRun.ProgressChanged += seekBarRunProgressChanged;
+
+			Button button01 = FindViewById<Button>(Resource.Id.Button01);
+			button01.Click += delegate { _button01Clicked = 1; };
+
+			Button button02 = FindViewById<Button>(Resource.Id.Button02);
+			button02.Click += delegate { _button02Clicked = 1; };
+
+			Button button03 = FindViewById<Button>(Resource.Id.Button03);
+			button03.Click += delegate { _button03Clicked = 1; };
+
+			Button button04 = FindViewById<Button>(Resource.Id.Button04);
+			button04.Click += delegate { _button04Clicked = 1; };
         }
 
         public bool OnUpdateAccelerometer(Accelerometer.Accelerometer accelerometer)
         {
             try
             {
-				NetWorkHelper.Send(string.Format("^{0}|{1}|{2}|0|0|0|0$", accelerometer.Rotation, progressBrake, progressRun));
+				NetWorkHelper.Send(string.Format("^{0}|{1}|{2}|{3}|{4}|{5}|{6}$", accelerometer.Rotation, progressBrake, progressRun, _button01Clicked, _button02Clicked, _button03Clicked, _button04Clicked));
+				_button01Clicked = 0;
+				_button02Clicked = 0;
+				_button03Clicked = 0;
+				_button04Clicked = 0;
             }
             catch (Exception ex)
             {
